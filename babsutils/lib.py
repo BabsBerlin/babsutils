26988d1382c1b1d888940c0cb7f44ff135874219
Barbara Hartmann <barbara.hartmann@gmail.com>
2022-02-20 18:22:16 +0100

    added sum function to lib module

--- babsutils/lib.py
+++ babsutils/lib.py
@@ -1,4 +1,9 @@
 
 def helloworld():
+    ''' simple greeting '''
     return "babsutils says hello world!"
+    
+def sum(a,b):
+    ''' return the sum of the parameters '''
+    return a + b
     
--- tests/test_lib.py
+++ tests/test_lib.py
@@ -3,3 +3,6 @@ from babsutils.lib import helloworld
 def test_length_of_helloworld():   
     assert len(helloworld()) != 0
     
+def test_calculation_of_sum():
+    assert sum(3,3) == 6
+    
